664aa7402cf6db68c9834139df92eb22861336fb
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2023-08-29 12:40:49 +0100

    getURLSFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const {JSDOM} = require('jsdom')
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const link of linkElements) { 
+        if (link.href.slice(0, 1) === '/') {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${link.href}`)
+                urls.push(urlObj.href)
+            }
+            catch (err) {
+                console.log(`error with relative url:  ${err.message}`)
+            }
+        }
+        else {
+            //absolute
+            try {
+                const urlObj = new URL(link.href)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with absolute url:  ${error.message}`)
+            }
+            
+        }
+    }
+    return urls
+}
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +37,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
